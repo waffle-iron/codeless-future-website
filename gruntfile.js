0c4b7b2c81ea87f7e44db0f10e429f79a35c43b8
Massimiliano Bertinetti <massimiliano@codelessfuture.com>
2017-04-04 16:50:33 +0200

    Deployng optimized site

--- gruntfile.js
+++ gruntfile.js
@@ -1,26 +1,19 @@
-
-rts = function (grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({
      pkg: grunt.file.readJSON('package.json'),
      cssmin: {
        minify: {
-         options: {
-           banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-         },
          expand: true,
-         src: ['*.css', '!*.min.css'],
-         dest: 'dist/',
-         ext: '.min.css'
+         src: ['css/*.css'],
+         dest: 'dist/'
        }
      },
      uglify: {
-       options: {
-         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-       },
        build: {
-         src: 'index.js',
-         dest: 'dist/index.min.js'
+         expand: true,
+         src: ['js/*.js'],
+         dest: 'dist/',
        }
      },
      processhtml: {
@@ -47,16 +40,35 @@ rts = function (grunt) {
            'dist/index.html': 'dist/index.min.html'
          }
        }
-     },
+    },
+    imagemin: {                          // Task
+    dynamic: {                         // Another target
+      files: [{
+        expand: true,                  // Enable dynamic expansion
+        cwd: 'img',                   // Src matches are relative to this path
+        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+        dest: 'dist/img'                  // Destination path prefix
+      }]
+    }
+},
+copy: {
+  main: {
+    expand: true,
+    src: 'css/fonts/*',
+    dest: 'dist/',
+  },
+},
 
      clean: ['dist*//*.min.*']
-   });
+ });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-processhtml');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.registerTask('default', ['cssmin','uglify', 'processhtml', 'htmlmin','clean']);
+  grunt.registerTask('default', ['cssmin','uglify', 'processhtml', 'htmlmin','imagemin','copy','clean']);
   grunt.registerTask('build', ['cssmin','uglify', 'htmlmin', 'processhtml']);
 };
