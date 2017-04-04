efb24e63d839d97c2630a471d1b7e21eb5a954fb
Massimiliano Bertinetti <massimiliano@codelessfuture.com>
2017-04-04 13:37:49 +0200

    first commit

---
+++ js/front.js
@@ -0,0 +1,149 @@
+if ($.cookie("theme_csspath")) {
+    $('link#theme-stylesheet').attr("href", $.cookie("theme_csspath"));
+}
+
+$(function () {
+
+    sticky();
+    map();
+    utils();
+   
+
+});
+
+
+
+function sticky() {
+
+    $(".header").sticky();
+
+}
+
+/* =========================================
+ *  map 
+ *  =======================================*/
+
+function map() {
+
+    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+    map = new GMaps({
+        el: '#map',
+        lat: 45.069272, 
+        lng: 7.668913,
+        zoomControl: true,
+        zoomControlOpt: {
+            style: 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        overviewMapControl: false,
+        scrollwheel: false,
+        draggable: false,
+        styles: styles
+    });
+
+    var image = 'img/marker.png';
+
+    map.addMarker({
+        lat: 45.069272, 
+        lng: 7.668913,
+        icon: image,
+        title: '',
+        infoWindow: {
+            content: '<p><b>Codeless Future</b><br/>Via Vittorio Amedeo II 21<br/>10122 Torino<br/>Italia</p>'
+        }
+    });
+}
+
+/* =========================================
+ *  UTILS
+ *  =======================================*/
+
+function utils() {
+
+    /* tooltips */
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* click on the box activates the radio */
+
+    $('#checkout').on('click', '.box.shipping-method, .box.payment-method', function (e) {
+        var radio = $(this).find(':radio');
+        radio.prop('checked', true);
+    });
+    /* click on the box activates the link in it */
+
+    $('.box.clickable').on('click', function (e) {
+
+        window.location = $(this).find('a').attr('href');
+    });
+    /* external links in new window*/
+
+    $('.external').on('click', function (e) {
+
+        e.preventDefault();
+        window.open($(this).attr("href"));
+    });
+    /* animated scrolling */
+
+    $('.scroll-to, #navigation a').click(function (event) {
+        event.preventDefault();
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+
+        $('body').scrollTo($('#' + trgt), 800, {offset: -50});
+
+    });
+
+}
+
+$.fn.alignElementsSameHeight = function () {
+    $('.same-height-row').each(function () {
+
+        var maxHeight = 0;
+        var children = $(this).find('.same-height');
+        children.height('auto');
+        if ($(window).width() > 768) {
+            children.each(function () {
+                if ($(this).innerHeight() > maxHeight) {
+                    maxHeight = $(this).innerHeight();
+                }
+            });
+            children.innerHeight(maxHeight);
+        }
+
+        maxHeight = 0;
+        children = $(this).find('.same-height-always');
+        children.height('auto');
+        children.each(function () {
+            if ($(this).innerHeight() > maxHeight) {
+                maxHeight = $(this).innerHeight();
+            }
+        });
+        children.innerHeight(maxHeight);
+    });
+}
+
+$(window).load(function () {
+
+    windowWidth = $(window).width();
+
+    $(this).alignElementsSameHeight();
+
+
+});
+$(window).resize(function () {
+
+    newWindowWidth = $(window).width();
+
+    if (windowWidth !== newWindowWidth) {
+        setTimeout(function () {
+            $(this).alignElementsSameHeight();
+        }, 100);
+        windowWidth = newWindowWidth;
+    }
+
+});
